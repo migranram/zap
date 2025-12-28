51c6faa2ffe801fca6e74cac794cab7bba3287e1
Miguel Granero <migranram@gmail.com>
2025-12-28 16:52:30 +0100

    change base numeric types to i64 and f64. and truncate to intcast

--- src/argument.zig
+++ src/argument.zig
@@ -8,8 +8,8 @@ fn BaseArg(T: type) type {
 }
 
 const BoolArg = BaseArg(bool);
-const IntArg = BaseArg(i32);
-const FloatArg = BaseArg(f32);
+const IntArg = BaseArg(i64);
+const FloatArg = BaseArg(f64);
 const StringArg = BaseArg([]const u8);
 
 pub const ParsingResult = enum { Parsed, NotParsed, AlreadyParsed };
@@ -21,7 +21,7 @@ pub const Argument = union(enum) {
     String: StringArg,
 
     pub fn createFromType(name: []const u8, T: type, value: ?T) errors.ParserError!Argument {
-        if (typing_utils.isInteger(T)) return Argument{ .Int = IntArg{ .name = name, .value = if (value) |v| @truncate(v) else undefined } };
+        if (typing_utils.isInteger(T)) return Argument{ .Int = IntArg{ .name = name, .value = if (value) |v| @intCast(v) else undefined } };
         if (typing_utils.isFloat(T)) return Argument{ .Float = FloatArg{ .name = name, .value = if (value) |v| @floatCast(v) else undefined } };
         if (T == bool) return Argument{ .Bool = BoolArg{ .name = name, .value = value orelse undefined } };
         if (T == []const u8) return Argument{ .String = StringArg{ .name = name, .value = value orelse undefined } };
@@ -58,7 +58,7 @@ pub const Argument = union(enum) {
     pub fn setValue(self: *Argument, value: anytype) !void {
         const T = @TypeOf(value);
         if (typing_utils.isInteger(T)) {
-            self.Int.value = @truncate(value);
+            self.Int.value = @intCast(value);
         } else if (typing_utils.isFloat(T)) {
             self.Float.value = @floatCast(value);
         } else if (T == bool) {
@@ -106,10 +106,10 @@ pub const Argument = union(enum) {
     pub fn parseValueFromString(self: *Argument, text: []const u8) errors.ParserError!void {
         switch (self.*) {
             .Int => {
-                try self.setValue(std.fmt.parseInt(i32, text, 10) catch return errors.ParserError.InvalidRawType);
+                try self.setValue(std.fmt.parseInt(i64, text, 10) catch return errors.ParserError.InvalidRawType);
             },
             .Float => {
-                try self.setValue(std.fmt.parseFloat(f32, text) catch return errors.ParserError.InvalidRawType);
+                try self.setValue(std.fmt.parseFloat(f64, text) catch return errors.ParserError.InvalidRawType);
             },
             .String => {
                 try self.setValue(text);

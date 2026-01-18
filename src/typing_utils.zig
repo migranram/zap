93db4278800d242967a2d70bc4681363381c7f0f
Miguel Granero <miguel.granero@iosb.fraunhofer.de>
2026-01-18 09:51:07 +0100

    fix: accept comptime_ types for isInt or isFloat utils

--- src/typing_utils.zig
+++ src/typing_utils.zig
@@ -1,12 +1,14 @@
 const std = @import("std");
 
 pub inline fn isInteger(T: type) bool {
+    if (T == comptime_int) return true;
     return switch (@typeInfo(T)) {
         .int => true,
         else => false,
     };
 }
 pub inline fn isFloat(T: type) bool {
+    if( T == comptime_float) return true;
     return switch (@typeInfo(T)) {
         .float => true,
         else => false,
